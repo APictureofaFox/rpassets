b79b8a1b6e72384d3b257d0b6de24acc088c1051
graystrappado <graystrappado@gmail.com>
2018-02-20 11:22:23 +0300

    base layout

--- src/rpassets/ui/Main.java
+++ src/rpassets/ui/Main.java
@@ -2,8 +2,6 @@ package rpassets.ui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import rpassets.core.model.numenera.Cypher;
 import rpassets.ui.view.EntityView;
@@ -12,14 +10,11 @@ import rpassets.ui.view.numenera.CypherView;
 public class Main extends Application {
 
     EntityView<Cypher> entityView = new CypherView();
-    ListView<Cypher> listView = entityView.getListView();
 
     @Override
     public void start(Stage primaryStage) {
         //Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
-        VBox root = new VBox();
-        primaryStage.setScene(new Scene(root, 300, 275));
-        root.getChildren().addAll(listView);
+        primaryStage.setScene(new Scene(entityView.getRoot()));
 
         primaryStage.setTitle("RPAssets");
         primaryStage.show();
--- src/rpassets/ui/view/EntityView.java
+++ src/rpassets/ui/view/EntityView.java
@@ -4,17 +4,37 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import rpassets.core.model.AssetEntity;
 import rpassets.core.model.ListModel;
-import rpassets.ui.Config;
 
 public abstract class EntityView<E extends AssetEntity> {
+    protected static final JsonPathsConfig config = JsonPathsConfig.INSTANCE;
     private ListModel<E> listModel;
     private ListView<E> listView;
     private ObservableList<E> items;
-    protected static final JsonPathsConfig config = JsonPathsConfig.INSTANCE;
+    private Pane root;
+    private Pane listBox;
+    private Pane entityWidget;
 
     public EntityView(String filepath) {
+        initializeModel(filepath);
+
+        this.listBox = new VBox(listView);
+        this.entityWidget = new Pane();
+        this.root = new HBox();
+
+        this.root.getChildren().addAll(listBox, entityWidget);
+        this.root.setPrefSize(900, 600);
+
+        this.entityWidget.prefHeightProperty().bind(this.root.heightProperty());
+        this.listBox.prefHeightProperty().bind(this.root.heightProperty());
+        this.listView.prefHeightProperty().bind(this.listBox.heightProperty());
+    }
+
+    private void initializeModel(final String filepath) {
         this.listModel = new ListModel<>(derivedClass(), filepath);
         this.items = FXCollections.observableArrayList(listModel.items());
         this.listView = new ListView<>();
@@ -22,13 +42,13 @@ public abstract class EntityView<E extends AssetEntity> {
         this.listView.setCellFactory(this::cellFactory);
     }
 
-    protected ListCell<E> cellFactory(ListView<E> list) {
+    private ListCell<E> cellFactory(ListView<E> list) {
         return new DefaultEntityListCell<>();
     }
 
     protected abstract Class<E> derivedClass();
 
-    public ListView<E> getListView() {
-        return listView;
+    public Pane getRoot() {
+        return this.root;
     }
 }
